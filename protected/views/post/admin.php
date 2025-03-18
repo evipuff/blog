bf6971bedb96de724c1a41d116681390938a505e
evipuff <ticsaysamantha@gmail.com>
2025-03-19 00:52:00 +0800

    first commit

---
+++ protected/views/post/admin.php
@@ -0,0 +1,51 @@
+<?php
+/* @var $this PostController */
+/* @var $model Post */
+
+$this->breadcrumbs=array(
+    'Manage Posts',
+);
+
+$this->menu=array(
+	array('label'=>'List Post', 'url'=>array('index')),
+	array('label'=>'Create Post', 'url'=>array('create')),
+);
+
+Yii::app()->clientScript->registerScript('search', "
+$('.search-button').click(function(){
+	$('.search-form').toggle();
+	return false;
+});
+$('.search-form form').submit(function(){
+	$('#post-grid').yiiGridView('update', {
+		data: $(this).serialize()
+	});
+	return false;
+});
+");
+?>
+
+<?php $this->widget('zii.widgets.grid.CGridView', array(
+    'dataProvider'=>$model->search(),
+    'filter'=>$model,
+    'columns'=>array(
+        array(
+            'name'=>'title',
+            'type'=>'raw',
+            'value'=>'CHtml::link(CHtml::encode($data->title), $data->url)'
+        ),
+        array(
+            'name'=>'status',
+            'value'=>'Lookup::item("PostStatus",$data->status)',
+            'filter'=>Lookup::items('PostStatus'),
+        ),
+        array(
+            'name'=>'create_time',
+            'type'=>'datetime',
+            'filter'=>false,
+        ),
+        array(
+            'class'=>'CButtonColumn',
+        ),
+    ),
+)); ?>
